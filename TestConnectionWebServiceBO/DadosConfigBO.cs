b70e7020cbacb9091ddabcf7377dc7894fc3f240
Anderson Junior Rodrigues <andersonkz@ymail.com>
2017-07-30 23:44:48 -0300

    Implementando metodo para leitura de arquivo xml

---
+++ TestConnectionWebServiceBO/DadosConfigBO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using TestConnectionWebServiceEntity;
+using TestConnectionWebServiceUtil;
+
+namespace TestConnectionWebServiceBO
+{
+    public class DadosConfigBO
+    {
+        private static string arquivoXml = @"D:\Visual Studio 2017\TestConnectionWebService\TestConnectionWebServiceWeb\Models\Config\DadosConfig.config";
+
+        public static DadosConfig BuscarDadosConfiguracao()
+        {
+            DadosConfig dados = new DadosConfig();
+            Util.GetDadosConfiguracao<DadosConfig>(ref dados);
+
+            return dados;
+        }
+    }
+}
---
+++ TestConnectionWebServiceEntity/DadosConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConnectionWebServiceEntity
+{
+    public class DadosConfig
+    {
+        public string Ativo { get; set; }
+        public string Caminho { get; set; }
+        public string Nome_Arquivo { get; set; }
+    }
+}
