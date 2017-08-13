4da177672359a42124e86a673108ef3eb292cfc3
Anderson Junior Rodrigues <andersonkz@ymail.com>
2017-08-12 22:48:37 -0300

    Implementando novos metodos e teste unitario

---
+++ TestConnectionWebServiceUnitTest/TestArquivo.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestConnectionWebServiceBO;
+using TestConnectionWebServiceEntity;
+
+namespace TestConnectionWebServiceUnitTest
+{
+    [TestClass]
+    public class TestArquivo
+    {
+        [TestMethod]
+        public void TestarArquivoExistente()
+        {
+            //Given
+            ArquivoBO arquivoBO = new ArquivoBO();
+            Arquivo arquivo = new Arquivo();
+            string nomeArquivo = "Horario GDS";
+
+            //When
+            arquivo = arquivoBO.BuscarArquivoPorNome(nomeArquivo);
+
+            //Then
+            Assert.AreEqual(nomeArquivo, arquivo.Nome);
+        }
+    }
+}
